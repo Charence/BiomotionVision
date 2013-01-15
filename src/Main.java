29f254c9cbcfbadcaa6fe8edca91c101c8891d24
Charence <charence@gmail.com>
2013-01-15 19:02:41 +0000

    ..

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ import static com.googlecode.javacv.cpp.opencv_video.*;
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
-
 public class Main {
 	// model background using Mixture of Gaussian (see paper)
 	private static BackgroundSubtractor background = new BackgroundSubtractorMOG2();
@@ -37,13 +36,17 @@ public class Main {
 	private static void addImage(String filename) {
 		IplImage image = cvLoadImage(filename);
 		if (image != null) {
-			frame.showImage(image);
+			//frame.showImage(image);
 			
 			// background subtraction
 			CvMat fgmask = CvMat.create(image.height(), image.width());
 			background.apply(image, fgmask, 0.2);
 			// subtract background from original image
-			//foreground = null;
+			CvMat bg = CvMat.create(image.height(), image.width());
+			background.getBackgroundImage(bg);
+			//frame.showImage(fgmask);
+			//frame.showImage
+			//ShowImage(image);
 			
 			// edge information
 			
@@ -58,5 +61,35 @@ public class Main {
 			cvReleaseImage(image);
 		}
 		//frame.dispose();
+	}
+	
+	private static void ShowImage(IplImage image, String caption) {
+		CvMat mat = image.asCvMat();
+		int width = mat.cols(); if(width < 1) width = 1;
+		int height = mat.rows(); if(height < 1) height = 1;
+		double aspect = 1.0 * width / height;
+		if(height < 128) { height = 128; width = (int) ( height * aspect ); }
+		if(width < 128) width = 128;
+		height = (int) ( width / aspect );
+		ShowImage(image, caption, width, height);
+	}
+
+	public static void ShowImage(IplImage image, String caption, int size) {
+		if(size < 128) size = 128;
+		CvMat mat = image.asCvMat();
+		int width = mat.cols(); if(width < 1) width = 1;
+		int height = mat.rows(); if(height < 1) height = 1;
+		double aspect = 1.0 * width / height;
+		if(height != size) { height = size; width = (int) ( height * aspect ); }
+		if(width != size) width = size;
+		height = (int) ( width / aspect );
+		ShowImage(image, caption, width, height);
+	}
+	
+	public static void ShowImage(IplImage image, String caption, int width, int height) {
+		CanvasFrame canvas = new CanvasFrame(caption, 1);   // gamma=1
+		canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		canvas.setCanvasSize(width, height);
+		canvas.showImage(image);
 	}
 }
