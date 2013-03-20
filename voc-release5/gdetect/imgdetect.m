b33d6f823938d6d7aacfb020b560eca0e8e2c464
cclw05 <cclw05@doc.ic.ac.uk>
2013-03-20 18:59:51 +0000

    comments

--- voc-release5/gdetect/imgdetect.m
+++ voc-release5/gdetect/imgdetect.m
@@ -10,7 +10,5 @@ function [ds, bs, trees] = imgdetect(im, model, thresh)
 %   thresh    Detection threshold (scores must be > thresh)
 
 im = color(im);
-imG = rgb2gray(im);
-imG = double(imG)/255.0;
 pyra = featpyramid(im, model); % image should be colour with double vals
 [ds, bs, trees] = gdetect(pyra, model, thresh);
